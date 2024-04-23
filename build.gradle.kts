347eb3d175e6008827b7745f5b778a2c829270f8
Salomon BRYS <salomon.brys@gmail.com>
2024-04-23 17:48:22 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("multiplatform") version "1.9.23"
+    id("org.jetbrains.compose") version "1.6.2"
+    id("net.kodein.cup") version "1.0.0-Beta-01"
+    id("net.kodein.gradle.resources.resource-files") version "1.0.0"
+}
+
+cup {
+    targetDesktop()
+    targetWeb()
+}
+
+kotlin {
+    sourceSets.commonMain {
+        dependencies {
+            implementation("net.kodein.themes:cup:2.0.0")
+
+            implementation(compose.material)
+            implementation("org.kodein.emoji:emoji-compose-m2:1.3.0")
+
+            implementation("io.github.alexzhirkevich:qrose:1.0.1")
+        }
+        resources.srcDirs(resourceFiles.outputDir)
+    }
+}
+
+tasks.withType<ProcessResources> {
+    dependsOn(tasks.importResourceFiles)
+}
+
+dependencies {
+    resourceFiles("net.kodein.themes:base-resources-font:2.0.0")
+}
